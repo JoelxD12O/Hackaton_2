9070f76791589db2ab778bdb9405133b2c0491a7
Joel Cayllahua <joel.game100xd@gmail.com>
2025-06-17 19:31:12 -0500

    feat: agregamos detailPage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import { useAuth } from "../contexts/AuthContext";
 import { useSummary } from "../hooks/useSummary";
 import SummaryView from "../components/Summary/SummaryView";
+import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
+  const navigate = useNavigate();
   const { token } = useAuth();
   const { summary, loading } = useSummary(token!);
 
@@ -13,6 +15,14 @@ export default function Homepage() {
     <div>
       <h1>Bienvenido a Ahorrista</h1>
       <SummaryView data={summary} />
+      <div className="mt-6">
+        <button
+          onClick={() => navigate('/detail')}
+          className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
+        >
+          Ver detalle de gastos por categoría
+        </button>
+      </div>
     </div>
   );
 }
